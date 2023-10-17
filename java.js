369926d5f6e520f4b1d3a10338c31e10c2346d5b
Matheus Chaves <143457612+zzMatheusChaveszz@users.noreply.github.com>
2023-10-17 19:20:41 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+// Declaração de duas variáveis inteiras e exibição da soma
+let x = 5;
+let y = 3;
+let soma = x + y;
+console.log("A soma de", x, "mais", y, "é igual a", soma);
+
+// Declaração de uma variável string e exibição do conteúdo
+let stringVar = "Bem vindo";
+console.log(stringVar);
+
+// Declaração de uma variável booleana
+let boolVar = true;
+console.log("O valor da variável booleana é:", boolVar);
+
+// Declaração de uma variável numérica e exibição do valor
+let numVar = 3.1415;
+console.log("O valor da variável numérica é:", numVar);
+
+
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Qual é a sua idade?");
+let bairro = prompt("Qual é o seu bairro?");
+
+nome = nome.toUpperCase();
+
+console.log("Nome em maiúsculas:", nome);
+console.log("Idade:", idade);
+console.log("Bairro:", bairro);
+
